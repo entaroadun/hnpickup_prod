41bad0fd58fd605e3a633d2e5807e946b673d872
pawelm <pawelm>
2016-09-14 20:35:03 -0700

    New idea

--- appengine_config.py
+++
@@ -1,6 +0,0 @@
-## this file and its content
-## is needed if you plan to 
-## copy data from 
-## one app to the other
-## (http://code.google.com/appengine/docs/adminconsole/datastoreadmin.html)
-remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = ('HTTP_X_APPENGINE_INBOUND_APPID', ['hnpickupdev'])
