b6c39b9fbbf8cdbee77d2331d752d3d3e52fca4e
pawelm <pawelm>
2016-09-15 22:48:43 -0700

    Making sure the structure matches datastore

--- get_hnpage.php
+++ get_hnpage.php
@@ -26,7 +26,7 @@ function get_dom_from_url ( $url ) {
 
 // =========================
 
-function get_posts_from_dom ( $dom ) {
+function get_posts_from_dom ( $dom, $page, $etime ) {
 
   $posts = array();
   // -----------------------
@@ -35,19 +35,30 @@ function get_posts_from_dom ( $dom ) {
     $elements = $finder->query("//tr[@class='athing']");
     if ( !is_null($elements) ) {
       foreach ( $elements as $element ) {
-	$id = $element->getAttribute('id');
+	// -- basic post information
+	$postid = $element->getAttribute('id');
 	$rank = preg_replace('/\.$/','',$element->childNodes->item(0)->childNodes->item(0)->textContent);
 	$title = $element->childNodes->item(3)->childNodes->item(0)->textContent;
 	$url = $element->childNodes->item(3)->childNodes->item(0)->getAttribute('href');
 	$points = preg_replace('/ points?$/','',$element->nextSibling->childNodes->item(1)->childNodes->item(1)->textContent);
 	$user = $element->nextSibling->childNodes->item(1)->childNodes->item(3)->textContent;
-	$time = preg_replace('/ ago$/','',$element->nextSibling->childNodes->item(1)->childNodes->item(5)->textContent);
-	if ( strlen($time) == 0 ) {
-	  $time = $points;
+	$posttime = preg_replace('/ ago$/','',$element->nextSibling->childNodes->item(1)->childNodes->item(5)->textContent);
+	// -- probably a hire post by ycombinator
+	if ( strlen($posttime) == 0 ) {
+	  $posttime = $points;
 	  $points = '0';
 	  $user = '';
 	}
-	$posts[$id] = array('rank'=>$rank,'id'=>$id,'title'=>$title,'url'=>$url,'points'=>$points,'user'=>$user,'time'=>$time,'compare'=>'0');
+	// -- convert time to minutes
+	if ( preg_match('/minute/',$posttime) ) {
+	  $posttime = preg_replace('/ minutes?/','',$posttime)*1;
+	} elseif ( preg_match('/hour/',$posttime) ) {
+	  $posttime = preg_replace('/ hours?/','',$posttime)*1*60;
+	} elseif ( preg_match('/day/',$posttime) ) {
+	  $posttime = preg_replace('/ days?/','',$posttime)*1*60*24;
+	}
+	// -- this should match datastore entities
+	$posts[$postid] = array('etime'=>$etime,'page'=>$page,'rank'=>$rank,'postid'=>$postid,'title'=>$title,'url'=>$url,'points'=>$points,'user'=>$user,'posttime'=>$posttime,'compare'=>'0');
       }
     }
   }
@@ -60,16 +71,19 @@ function get_posts_from_dom ( $dom ) {
 
 function compare_posts ( &$posts_ref_1, &$posts_ref_2 ) {
 
+  $the_same = 0;
   // -----------------------
   if ( count($posts_ref_1) && count($posts_ref_2) ) {
     foreach ( $posts_ref_1 as $key => $post ) {
       if ( array_key_exists($key,$posts_ref_2) ) {
 	$posts_ref_1[$key]['compare'] = 1;
 	$posts_ref_2[$key]['compare'] = 1;
+	$the_same ++;
       }
     }
   }
   // -----------------------
+  return($the_same);
 
 }
 
@@ -80,10 +94,13 @@ function insert_posts_into_datastore ( $posts ) {
 
 // =========================
 
+$etime = time();
 $news_dom = get_dom_from_url('https://news.ycombinator.com/news');
-$news_posts = get_posts_from_dom($news_dom);
+$news_posts = get_posts_from_dom($news_dom,'news',$etime);
 $newest_dom = get_dom_from_url('https://news.ycombinator.com/newest');
-$newest_posts = get_posts_from_dom($newest_dom);
-compare_posts($news_posts,$newest_posts);
+$newest_posts = get_posts_from_dom($newest_dom,'newest',$etime);
+$the_same = compare_posts($news_posts,$newest_posts);
+echo "Parsed ".count($news_posts)." news and ".count($newest_posts)." newest posts where ".$the_same." is/are the same at the time ".$etime." ...\n";
+
 
 ?>
