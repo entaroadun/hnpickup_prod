62ca17d5a919a41e8facc9db7c1fb7aec54bd3e1
pawelm <pawelm>
2016-09-15 21:54:40 -0700

    Parsing HN pages

--- get_hnpage.php
+++ get_hnpage.php
@@ -1,31 +1,89 @@
 <?php
 
-echo "Work in progress ... stayed tuned ...";
+echo "Work in progress ... stayed tuned ...\n";
 
-// GET "HACKER NEWS" LATEST NEWS
-$curl = curl_init();
-curl_setopt_array($curl,array(CURLOPT_RETURNTRANSFER=>1,CURLOPT_URL=>'https://news.ycombinator.com/news', CURLOPT_USERAGENT=>'HNPICKUP SERVICE'));
-$news = curl_exec($curl);
-curl_close($curl);
+// =========================
 
-// GET "HACKER NEWS" NEWEST POSTS
-curl_setopt_array($curl,array(CURLOPT_RETURNTRANSFER=>1,CURLOPT_URL=>'https://news.ycombinator.com/newest',CURLOPT_USERAGENT=>'HNPICKUP SERVICE'));
-$newest = curl_exec($curl);
-curl_close($curl);
+function get_dom_from_url ( $url ) {
 
-// PARSE RESULTS
-//<tr class='athing' id='12502991'> <td align="right" valign="top" class="title"><span class="rank">18.</span></td>      <td valign="top" class="votelinks"><center><a id='up_12502991' onclick='return vote(this, "up")' href='vote?id=12502991&amp;how=up&amp;auth=be91e9da3a1bfca76e28874478701cd716f713a6&amp;goto=newest'><div class='votearrow' title='upvote'></div></a></center></td><td class="title"><a href="https://github.com/kaienkira/acme-client" class="storylink" rel="nofollow">A small PHP script to get and renew TLS certs from Let's Encrypt</a><span class="sitebit comhead"> (<a href="from?site=github.com"><span class="sitestr">github.com</span></a>)</span></td></tr><tr><td colspan="2"></td><td class="subtext"> <span class="score" id="score_12502991">1 point</span> by <a href="user?id=kaienkira" class="hnuser"><font color="#3c963c">kaienkira</font></a> <span class="age"><a href="item?id=12502991">1 hour ago</a></span> <span id="unv_12502991"></span> | <a href="flag?id=12502991&amp;auth=be91e9da3a1bfca76e28874478701cd716f713a6&amp;goto=newest">flag</a> | <a href="hide?id=12502991&amp;goto=newest&amp;auth=be91e9da3a1bfca76e28874478701cd716f713a6" onclick="return hidestory(this, 12502991)">hide</a> | <a href="https://hn.algolia.com/?query=A%20small%20PHP%20script%20to%20get%20and%20renew%20TLS%20certs%20from%20Let's%20Encrypt&sort=byDate&dateRange=all&type=story&storyText=false&prefix&page=0" class="hnpast">past</a> | <a href="https://www.google.com/search?q=A%20small%20PHP%20script%20to%20get%20and%20renew%20TLS%20certs%20from%20Let's%20Encrypt">web</a> | <a href="item?id=12502991">discuss</a>              </td></tr>
-
-$NEWS_ARR = array();
-
-function get_matches_in_array ( $matches ) {
-  array_push($NEWS_ARR,$matches[1]);
+  $dom = null;
+  // -----------------------
+  $ch = curl_init(); 
+  curl_setopt($ch,CURLOPT_URL,$url); 
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1); 
+  $html = curl_exec($ch); 
+  curl_close($ch); 
+  if ( strlen($html) > 0 ) {
+    $dom = new domDocument; 
+    $internalErrors = libxml_use_internal_errors(true);
+    $dom->loadHTML($html);
+    libxml_use_internal_errors($internalErrors);
+  }
+  // -----------------------
+  return($dom);
 
 }
 
-$string = preg_replace_callback('/(\d+) points?<\/span> by/','get_matches_in_array',$news);
+// =========================
 
+function get_posts_from_dom ( $dom ) {
 
-echo " ".strlen($news)." ".strlen($newest)." ".$NEWS_ARR[0]."|";
+  $posts = array();
+  // -----------------------
+  if ( !is_null($dom) ) {
+    $finder = new DomXPath($dom);
+    $elements = $finder->query("//tr[@class='athing']");
+    if ( !is_null($elements) ) {
+      foreach ( $elements as $element ) {
+	$id = $element->getAttribute('id');
+	$rank = preg_replace('/\.$/','',$element->childNodes->item(0)->childNodes->item(0)->textContent);
+	$title = $element->childNodes->item(3)->childNodes->item(0)->textContent;
+	$url = $element->childNodes->item(3)->childNodes->item(0)->getAttribute('href');
+	$points = preg_replace('/ points?$/','',$element->nextSibling->childNodes->item(1)->childNodes->item(1)->textContent);
+	$user = $element->nextSibling->childNodes->item(1)->childNodes->item(3)->textContent;
+	$time = preg_replace('/ ago$/','',$element->nextSibling->childNodes->item(1)->childNodes->item(5)->textContent);
+	if ( strlen($time) == 0 ) {
+	  $time = $points;
+	  $points = '0';
+	  $user = '';
+	}
+	$posts[$id] = array('rank'=>$rank,'id'=>$id,'title'=>$title,'url'=>$url,'points'=>$points,'user'=>$user,'time'=>$time,'compare'=>'0');
+      }
+    }
+  }
+  // -----------------------
+  return($posts);
+       
+}
+
+// =========================
+
+function compare_posts ( &$posts_ref_1, &$posts_ref_2 ) {
+
+  // -----------------------
+  if ( count($posts_ref_1) && count($posts_ref_2) ) {
+    foreach ( $posts_ref_1 as $key => $post ) {
+      if ( array_key_exists($key,$posts_ref_2) ) {
+	$posts_ref_1[$key]['compare'] = 1;
+	$posts_ref_2[$key]['compare'] = 1;
+      }
+    }
+  }
+  // -----------------------
+
+}
+
+// =========================
+
+function insert_posts_into_datastore ( $posts ) {
+}
+
+// =========================
+
+$news_dom = get_dom_from_url('https://news.ycombinator.com/news');
+$news_posts = get_posts_from_dom($news_dom);
+$newest_dom = get_dom_from_url('https://news.ycombinator.com/newest');
+$newest_posts = get_posts_from_dom($newest_dom);
+compare_posts($news_posts,$newest_posts);
 
 ?>
