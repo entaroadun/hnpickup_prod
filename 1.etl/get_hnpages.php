c8f48f0e17037de2aeda685288e123c34b1df5cc
pawelm <pawelm>
2016-09-20 23:51:27 -0700

    Fixing some bugs; local dev; table to show rewing results

--- 1.etl/get_hnpages.php
+++ 1.etl/get_hnpages.php
@@ -24,8 +24,11 @@ function get_dom_from_url ( $url ) {
   // -----------------------
   $ch = curl_init(); 
   if ( !is_null($ch) ) {
+    // -- this is convert to "url fetch" by GAE
     curl_setopt($ch,CURLOPT_URL,$url); 
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1); 
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); 
+    curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
+    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
     $html = curl_exec($ch); 
     curl_close($ch); 
     // -- convert text to DOM structure
--- 3.rep/app.php
+++ 3.rep/app.php
@@ -14,22 +14,56 @@ use Symfony\Component\HttpFoundation\Response;
 $APP = new Silex\Application();
 $APP->register(new Silex\Provider\TwigServiceProvider());
 $APP['twig.path'] = [ __DIR__.'/templates' ];
+$APP['hnposts'] = new GDS\Store('HNPOSTS');
+$APP['hnposts_summary'] = new GDS\Store('HNPOSTS_SUMMARY');
+date_default_timezone_set('America/Los_Angeles');
 
 // =========================
 
 $APP->get('/', function (Application $app, Request $request) {
-  return($app->redirect('/report/dashboard'));
+  return($app->redirect('/report/dashboard/news/1'));
 });
 
-$APP->get('/report/dashboard', function (Application $app, Request $request) {
-  return($app['twig']->render('dashboard.html',array('report_name'=>'Full Dashboard')));
+$APP->get('/report/dashboard/{page}/{number}', function ($page, $number, Application $app, Request $request) {
+  $memcache = new Memcached;
+  $number = 1*$number;
+  if ( !$memcache->get('hnposts'.$number.$page) ) {
+    $table_rows = array();
+    $table_name = '';
+    $table_next = '';
+    $table_prev = '';
+    $hnposts = $app['hnposts'];
+    $hnposts->query('SELECT * FROM HNPOSTS ORDER BY etime DESC LIMIT @end OFFSET @start',['end'=>60,'start'=>(($number-1)*60)]);
+    $rows = $hnposts->fetchAll();
+    usort($rows,function($a,$b){return(($a->rank<$b->rank)?-1:1);});
+    foreach ( $rows as $row ) {
+      if ( $row->page == $page ) {
+        $table_rows[] = ['rank'=>$row->rank,'title'=>$row->title,'url'=>$row->url,'points'=>$row->points,'user'=>$row->user,'compare'=>$row->compare];
+        if ( strcmp($table_name,'') === 0 ) {
+          $table_name = date('Y-m-d H:i T',(int)$row->etime);
+        }
+      }
+    }
+    $table_name = 'Hacker '.ucfirst($page).' Page at '.$table_name;
+    $table_next = "<a href='/report/dashboard/$page/".($number-1)."'>&gt;</a>";
+    $table_prev = "<a href='/report/dashboard/$page/".($number+1)."'>&lt;</a>";
+    $table = ['table_rows'=>$table_rows,'table_prev'=>$table_prev,'table_next'=>$table_next,'table_name'=>$table_name];
+    $memcache->set('hnposts'.$number.$page,$table);
+  } else {
+    $table = $memcache->get('hnposts'.$number.$page);
+  }
+  $table_name = $table['table_name'];
+  $table_prev = $table['table_prev'];
+  $table_next = $table['table_next'];
+  $table_rows = $table['table_rows'];
+  return($app['twig']->render('dashboard.html',array('report_name'=>'Full Dashboard','table_rows'=>$table_rows,'table_name'=>$table_name,'table_prev'=>$table_prev,'table_next'=>$table_next)));
 });
 
-$APP->get('/report/news', function (Application $app, Request $request) {
+$APP->get('/report/news/{number}', function ($number, Application $app, Request $request) {
   return($app['twig']->render('news.html',array('report_name'=>'Hacker News Page Rewind')));
 });
 
-$APP->get('/report/newest', function (Application $app, Request $request) {
+$APP->get('/report/newest/{number}', function ($number, Application $app, Request $request) {
   return($app['twig']->render('newest.html',array('report_name'=>'Hacker Newest Page Rewind')));
 });
 
