e90d075eb43bb007daba3083e21b5f544ac8b8b4
pawelm <pawelm>
2016-09-23 00:39:44 -0700

    Moving to ajax/json data

--- 3.rep/app.php
+++ 3.rep/app.php
@@ -11,82 +11,115 @@ putenv('GOOGLE_APPLICATION_CREDENTIALS=../0.conf/hnpickup_key.json');
 
 // =========================
 
+function get_hnposts ( $hnposts, $offset, $page ) {
+
+  $table_rows = array();
+  // -----------------------
+  if ( !is_null($hnposts) && is_numeric($offset) && ( $page == 'news' || $page == 'newest' ) ) {
+    $hnposts->query('SELECT * FROM HNPOSTS WHERE page = @page ORDER BY etime DESC LIMIT @limit OFFSET @offset',['limit'=>30,'offset'=>(($offset-1)*30),'page'=>$page]);
+    $rows = $hnposts->fetchAll();
+    usort($rows,function($a,$b){return(($a->rank<$b->rank)?-1:1);});
+    foreach ( $rows as $row ) {
+      $table_rows[] = ['rank'=>$row->rank,'title'=>$row->title,'url'=>$row->url,'points'=>$row->points,'postid'=>$row->postid,'compare'=>$row->compare,'etime'=>$row->etime];
+    }
+  }
+  // -----------------------
+  return($table_rows);
+
+}
+
+// =========================
+
+function get_summary ( $hnposts, $offset, $limit, $field ) {
+
+  $data_rows = array();
+  // -----------------------
+  if ( !is_null($hnposts) && is_numeric($offset) && is_numeric($limit) ) {
+    $hnposts->query('SELECT * FROM HNPOSTS_SUMMARY ORDER BY etime DESC LIMIT @limit OFFSET @offset',['limit'=>$limit,'offset'=>($offset-1)]);
+    $rows = array_reverse($hnposts->fetchAll());
+    $i = min($limit,count($rows));
+    foreach ( $rows as $row ) {
+      $value = $row->newest_max-$row->news_min;
+      $value_sign = ($value>=0) ? 1 : -1;
+      $log_value = round(log(abs($value)+1,2),3)*$value_sign;
+      $data_rows[] = ['etime'=>($row->etime)*1,'ratio'=>$log_value,'offset'=>$offset+$i-1];
+      $i --;
+    }
+  }
+  // -----------------------
+  return($data_rows);
+
+}
+
+// =========================
+
 // create the Silex application
 $APP = new Silex\Application();
 $APP->register(new Silex\Provider\TwigServiceProvider());
 $APP['twig.path'] = [ __DIR__.'/templates' ];
 $APP['hnposts'] = new \GDS\Store('HNPOSTS');#,new \GDS\Gateway\RESTv1('hnpickup'));
-$APP['hnposts_summary'] = new \GDS\Store('HNPOSTS_SUMMARY');#,new \GDS\Gateway\RESTv1('hnpickup'));
+$APP['summary'] = new \GDS\Store('HNPOSTS_SUMMARY');#,new \GDS\Gateway\RESTv1('hnpickup'));
 date_default_timezone_set('America/Los_Angeles');
 
 // =========================
 
 $APP->get('/', function (Application $app, Request $request) {
-  return($app->redirect('/report/dashboard/1'));
+  return($app->redirect('/report/dashboard'));
 });
 
 // =========================
 
-$APP->get('/report/dashboard/{number}', function ($number, Application $app, Request $request) {
+$APP->get('/hnposts/{offset}/{page}.json', function ( $offset, $page, Application $app, Request $request ) {
   $memcache = new Memcached;
-  $number = 1*$number;
-  if ( !$memcache->get('hnposts.'.$number) ) {
-    $table_rows = array();
-    $data_rows = array();
-    $table_date = '';
-    $table_next = '';
-    $table_prev = '';
+  // - - - - - - - - -
+  if ( !($table_rows = $memcache->get("hnposts/$offset/$page")) ) {
     $hnposts = $app['hnposts'];
-    // - - - - - - - - - -
-    $hnposts->query('SELECT * FROM HNPOSTS ORDER BY etime DESC LIMIT @end OFFSET @start',['end'=>60,'start'=>(($number-1)*60)]);
-    $rows = $hnposts->fetchAll();
-    usort($rows,function($a,$b){return(($a->rank<$b->rank)?-1:1);});
-    foreach ( $rows as $row ) {
-      $table_rows[$row->page][] = ['rank'=>$row->rank,'title'=>$row->title,'url'=>$row->url,'points'=>$row->points,'postid'=>$row->postid,'compare'=>$row->compare];
-      if ( strcmp($table_date,'') === 0 ) {
-        $table_date = date('Y-m-d H:i T',(int)$row->etime);
-      }
-    }
-    $table_next = "/report/dashboard/".($number-1);
-    $table_prev = "/report/dashboard/".($number+1);
-    // - - - - - - - - - -
-    $hnposts->query('SELECT * FROM HNPOSTS_SUMMARY ORDER BY etime DESC LIMIT @end OFFSET @start',['end'=>60,'start'=>($number-1)]);
-    $rows = array_reverse($hnposts->fetchAll());
-    foreach ( $rows as $row ) {
-      $value = $row->newest_max-$row->news_min;
-      $value_sign = ($value>=0) ? 1 : -1;
-      $log_value = round(log(abs($value)+1,2),3)*$value_sign;
-      $data_rows[] = ['etime'=>$row->etime,'value'=>$log_value];
-    }
-    // - - - - - - - - - -
-    $table = ['table_news'=>$table_rows['news'],'table_newest'=>$table_rows['newest'],'table_prev'=>$table_prev,'table_next'=>$table_next,'table_date'=>$table_date,'etime'=>time(),'dataset'=>$data_rows];
-    $memcache->set('hnposts.'.$number,$table);
-    $memcache->delete('hnposts.'.$number,600);
-  } else {
-    $table = $memcache->get('hnposts.'.$number);
+    $table_rows = get_hnposts($hnposts,$offset,$page);
+    $memcache->set("hnposts/$offset/$page",$table_rows,time()+15*60);
   }
-  $table_date = $table['table_date'];
-  $table_prev = $table['table_prev'];
-  $table_next = $table['table_next'];
-  $table_news = $table['table_news'];
-  $table_newest = $table['table_newest'];
-  $dataset = $table['dataset'];
-  return($app['twig']->render('dashboard.html',array('report_name'=>'Full Dashboard','table_news'=>$table_news,'table_newest'=>$table_newest,'table_date'=>$table_date,'table_prev'=>$table_prev,'table_next'=>$table_next,'dataset'=>$dataset)));
+  // - - - - - - - - -
+  return(json_encode($table_rows));
 });
 
 // =========================
 
-$APP->get('/report/news/{number}', function ($number, Application $app, Request $request) {
+$APP->get('/summary/{offset}/{limit}/{field}.json', function ( $offset, $limit, $field, Application $app, Request $request ) {
+  $memcache = new Memcached;
+  // - - - - - - - - -
+  if ( !($table_rows = $memcache->get("summary/$offset/$limit/$field")) ) {
+    $summary = $app['summary'];
+    $table_rows = get_summary($summary,$offset,$limit,$field);
+    $memcache->set("summary/$offset/$limit/$field",$table_rows,time()+15*60);
+  }
+  // - - - - - - - - -
+  return(json_encode($table_rows));
+});
+
+// =========================
+
+$APP->get('/report/dashboard', function ( Application $app, Request $request ) {
+  return($app['twig']->render('dashboard.html',array('report_name'=>'Full Dashboard')));
+});
+
+// =========================
+
+$APP->get('/report/news', function ( Application $app, Request $request) {
   return($app['twig']->render('news.html',array('report_name'=>'Hacker News Page Rewind')));
 });
 
-$APP->get('/report/newest/{number}', function ($number, Application $app, Request $request) {
+// =========================
+
+$APP->get('/report/newest', function ( Application $app, Request $request ) {
   return($app['twig']->render('newest.html',array('report_name'=>'Hacker Newest Page Rewind')));
 });
 
-$APP->get('/report/pickup', function (Application $app, Request $request) {
+// =========================
+
+$APP->get('/report/pickup', function ( Application $app, Request $request ) {
   return($app['twig']->render('pickup.html',array('report_name'=>'Hacker News Pickup Ratio')));
 });
+
+// =========================
 
 $APP->error(function (Exception $e, Request $request, $code) use ($APP) {
   switch ( $code ) {
